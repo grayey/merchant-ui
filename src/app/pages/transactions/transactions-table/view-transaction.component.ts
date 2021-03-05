894c1c98bcc53ac3dad7f15ae6bfe374da6c3f52
Emeka <bluegraonics@gmail.com>
2021-03-05 01:06:27 +0100

    Added print on transaction

--- src/app/pages/transactions/transactions-table/view-transaction.component.ts
+++ src/app/pages/transactions/transactions-table/view-transaction.component.ts
@@ -26,8 +26,32 @@ export class ViewTransactionComponent implements OnInit{
   
     ngOnInit ():void {
   
-      console.log('DEFAULTS', this.transaction)
+    //   console.log('DEFAULTS', this.transaction)
   
     }
+
+    public printTransaction(containerId) {
+      
+        document.getElementById('print_button').remove()
+
+        const mywindow = window.open('', 'PRINT', 'height=400,width=600');
+    
+
+        mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+        mywindow.document.write('</head><body >');
+        mywindow.document.write('<h1>' + document.title  + '</h1>');
+        mywindow.document.write(document.getElementById(containerId).innerHTML);
+        mywindow.document.write('</body></html>');
+    
+        mywindow.document.close(); // necessary for IE >= 10
+        mywindow.focus(); // necessary for IE >= 10*/
+    
+        mywindow.print();
+        mywindow.close();
+    
+        return true;
+    
+    
+      }
   
   }
